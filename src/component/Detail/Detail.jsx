2fd8d88c77276aa40ab672c8bd53e3422f749afc
Sandro <sandro_malca@usmp.pe>
2023-02-16 11:14:15 -0500

    add componente Detail

--- src/component/Detail/Detail.jsx
+++ src/component/Detail/Detail.jsx
@@ -1,26 +1,90 @@
-import React from "react";
-import styles  from "./Detail.module.css";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import styles from "./Detail.module.css";
+import { useParams } from "react-router-dom";
 
-export default function Detail(props){
-    const {title,instructor,duration,updated,description,modules,categoria,image}=props
-    return(
-        <div className={styles.container}>
+export default function Detail(props) {
+  const { detailId } = useParams();
+  const [course, setcourse] = useState([]);
+  console.log(detailId);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/courses/detail/${detailId}`)
+      .then((response) => response.json())
+      .then((curso) => {
+        if (curso.Title) {
+          console.log("------>", curso);
+          setcourse(curso);
+        } else {
+          window.alert("No hay personajes con ese ID");
+        }
+      })
+      .catch((err) => {
+        window.alert("No hay personajes con ese ID");
+      });
+    return setcourse([]);
+  }, [detailId]);
+  
+  return(
+      <div className={styles.container}>
             <div className={styles.contaimerTitle}>
-                <h1>{title}</h1>
-                <h2>{categoria}</h2>
-            </div>
-            <div className={styles.containerImg}>{image}</div>
-            <div className={styles.containerSummary}>
-                <h2>{instructor}</h2>
-                <h2>{duration}</h2>
-                <h2>{updated}</h2>
-            </div>
-            <div className={styles.containerDescription}>{description}</div>
-            <div className={styles.containerModules}>{modules}</div>
-            <div className={styles.containerBtn}>
-                <Link to={`/detail/${props.id}`}><button>Study</button></Link>
+                    <h1>{course.Title}</h1>
+                    <h2>{course.Categoria}</h2>
                 </div>
-        </div>
-    )
+                <div className={styles.containerImg}>{course.Image}</div>
+                <div className={styles.containerSummary}>
+                    <h2>{course.Instructor}</h2>
+                    <h2>{course.Duration}</h2>
+                    <h2>{course.Updated}</h2>
+                </div>
+            <div className={styles.containerDescription}>{course.Description}</div>
+            <div className={styles.containerModules}>{course.Modules}</div>
+            {/* <div className={styles.containerBtn}>
+                <Link to={`/detail/${props.id}`}><button>Study</button></Link>
+            </div> */}
+      </div>
+  )
 }
+
+//   return (
+//     <div className={styles.detail}>
+//       <div className={styles.txt}>
+//         <h1>{course.name}</h1>
+//         <h3>{course.status}</h3>
+//         <p>{course.species}</p>
+//         <p>{course.gender}</p>
+//         <p>{course.origin?.name}</p>
+//       </div>
+//       <img className={styles.imgD} src={course.image} alt={course.name} />
+//     </div>
+//   );
+
+
+
+
+// import React from "react";
+// import styles  from "./Detail.module.css";
+// import { Link } from "react-router-dom";
+
+// export default function Detail(props){
+//     const {title,instructor,duration,updated,description,modules,categoria,image}=props
+//     return(
+//         <div className={styles.container}>
+//             <div className={styles.contaimerTitle}>
+//                 <h1>{title}</h1>
+//                 <h2>{categoria}</h2>
+//             </div>
+//             <div className={styles.containerImg}>{image}</div>
+//             <div className={styles.containerSummary}>
+//                 <h2>{instructor}</h2>
+//                 <h2>{duration}</h2>
+//                 <h2>{updated}</h2>
+//             </div>
+//             <div className={styles.containerDescription}>{description}</div>
+//             <div className={styles.containerModules}>{modules}</div>
+//             <div className={styles.containerBtn}>
+//                 <Link to={`/detail/${props.id}`}><button>Study</button></Link>
+//                 </div>
+//         </div>
+//     )
+// }
